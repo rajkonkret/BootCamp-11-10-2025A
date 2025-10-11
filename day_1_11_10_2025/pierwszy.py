99b929510a3c4f015b6136be58e9bcacbe978207
radoslawjaniak <rajkokret660@gmail.com>
2025-10-11 11:33:21 +0200

    [1 6] update pierwszy.py

--- day_1_11_10_2025/pierwszy.py
+++ day_1_11_10_2025/pierwszy.py
@@ -3,3 +3,25 @@
 # snake_case - konwencja nazewnicza
 # ctrl alt l
 
+print()  # wypisz/wydrukuj
+
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+# ctrl d - powielanie linii
+
+print('Nazywam się Radek')  # Nazywam się Radek
+# Process finished with exit code 0 - program zakończył się poprawnie
+
+# ctrl / - komentowanie linijki
+# print('Nazywam się Radek")
+#   File "/Users/radoslawjaniak/PycharmProjects/BootCamp-11-10-2025A/day_1_11_10_2025/pierwszy.py", line 19
+#     print('Nazywam się Radek")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 19)
+#
+# Process finished with exit code 1 - bład wykonania programu
+print("Dalsza część programu")
