62f14c9f3310131aaf67862a55efa3cc607b7cc2
radoslawjaniak <rajkokret660@gmail.com>
2025-10-11 14:13:58 +0200

    [1 20] update formaty.py

--- day_1_11_10_2025/formaty.py
+++ day_1_11_10_2025/formaty.py
@@ -3,3 +3,12 @@ wiek = 41  # int
 wersja = 3.900001
 print(type(wersja))  # <class 'float'>, zmiennoprzecikowe
 liczba = 890789678567456  # int
+
+print("Witaj %s masz teraz %d lat." % (user, wiek))  # Witaj Tomek masz teraz 41 lat.
+# print("Witaj %s masz teraz %d lat." % (wiek, user)) # TypeError: %d format: a real number is required, not str
+# print("Witaj %d masz teraz %s lat." % (user, wiek)) # TypeError: %d format: a real number is required, not str
+# %d: formatowanie liczb całkowitych
+# %f: formatowanie liczb zmiennoprzecinkowych
+# %s - łańcuch znaków (string)
+
+
