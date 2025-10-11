296e95f835004014f84f01de460dfa1aac49343d
radoslawjaniak <rajkokret660@gmail.com>
2025-10-11 12:02:01 +0200

    [1 8] update pierwszy.py

--- day_1_11_10_2025/pierwszy.py
+++ day_1_11_10_2025/pierwszy.py
@@ -55,3 +55,38 @@ print(sys.int_info)
 #              default_max_str_digits=4300,
 #              str_digits_check_threshold=640)
 
+# zmienna - pudełko na dane
+# snake_case
+# wg PEP8 nazwa zmiennej powinna podpowiadać co zawiera
+
+# typowanie dynamiczne
+# mozna do zmiennej dowolny typ danych wrzucić w każdej chwili
+name = "Radek"  # wpisanie wartości do zmiennej
+print(type(name))  # <class 'str'>
+print(name)  # Radek - wypisanie zawartości zmiennej
+
+name = 90
+print(type(name))  # <class 'int'>
+print(name)  # 90
+
+name_info = "Radek"  #
+# print(19 + name_info)  # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# podpowiedzi typu
+name_info: str = "Maciek"
+print(name_info)  # Maciek
+name_info = 90
+print(name_info)  # 90
+
+# pip install mypy - instalacja programu - sprawdza zgodność typów  w programie pythonowym
+# cd day_1_11_10_2025  wejście do katalogu
+# (.venv) radoslawjaniak@mac day_1_11_10_2025 % mypy pierwszy.py
+# pierwszy.py:67: error: Incompatible types in assignment (expression has type "int", variable has type "str")  [assignment]
+# pierwszy.py:75: error: Name "name_info" already defined on line 71  [no-redef]
+# pierwszy.py:77: error: Incompatible types in assignment (expression has type "int", variable has type "str")  [assignment]
+# Found 3 errors in 1 file (checked 1 source file)
+# (.venv) radoslawjaniak@mac day_1_11_10_2025 %
+
+age = 48
+print(age)  # 48
+print(type(age))  # <class 'int'>
