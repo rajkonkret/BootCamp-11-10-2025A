ae222d541896c5945edadeca8a111991311e4157
radoslawjaniak <rajkokret660@gmail.com>
2025-10-11 14:26:12 +0200

    [1 21] update formaty.py

--- day_1_11_10_2025/formaty.py
+++ day_1_11_10_2025/formaty.py
@@ -4,6 +4,7 @@ wersja = 3.900001
 print(type(wersja))  # <class 'float'>, zmiennoprzecikowe
 liczba = 890789678567456  # int
 
+# ten sposób typy są sprawdzane
 print("Witaj %s masz teraz %d lat." % (user, wiek))  # Witaj Tomek masz teraz 41 lat.
 # print("Witaj %s masz teraz %d lat." % (wiek, user)) # TypeError: %d format: a real number is required, not str
 # print("Witaj %d masz teraz %s lat." % (user, wiek)) # TypeError: %d format: a real number is required, not str
@@ -11,4 +12,17 @@ print("Witaj %s masz teraz %d lat." % (user, wiek))  # Witaj Tomek masz teraz 41
 # %f: formatowanie liczb zmiennoprzecinkowych
 # %s - łańcuch znaków (string)
 
+print("Witaj %(user)s, masz teraz %(wiek)d lat." % {'user': user, "wiek": wiek})
+# Witaj Tomek, masz teraz 41 lat.
+print("Witaj %(user)s, masz teraz %(age)d lat." % {'user': user, "age": wiek})
+# Witaj Tomek, masz teraz 41 lat.
+print("Witaj %(user)s, masz teraz %(age)d lat, miło cię widzieć %(user)s" % {'user': user, "age": wiek})
+# Witaj Tomek, masz teraz 41 lat, miło cię widzieć Tomek
 
+# zrobić za pomocą f-string
+print(f"Witaj {user}, masz teraz {wiek} lat, miło cię widzieć {user}.")
+# Witaj Tomek, masz teraz 41 lat, miło cię widzieć Tomek.
+
+# nie sprawdza typów
+print(f"Witaj {wiek}, masz teraz {wiek} lat, miło cię widzieć {user}.")
+# Witaj 41, masz teraz 41 lat, miło cię widzieć Tomek.
