b09dd5af21957e2a4cf9a262386ac0586c1f38aa
radoslawjaniak <rajkokret660@gmail.com>
2025-10-11 14:34:40 +0200

    [1 22] update formaty.py

--- day_1_11_10_2025/formaty.py
+++ day_1_11_10_2025/formaty.py
@@ -26,3 +26,30 @@ print(f"Witaj {user}, masz teraz {wiek} lat, miło cię widzieć {user}.")
 # nie sprawdza typów
 print(f"Witaj {wiek}, masz teraz {wiek} lat, miło cię widzieć {user}.")
 # Witaj 41, masz teraz 41 lat, miło cię widzieć Tomek.
+
+print("Używamy wersji Pythona %i" % 3)  # Używamy wersji Pythona 3
+print("Używamy wersji Pythona %f" % 3)  # Używamy wersji Pythona 3.000000
+print("Używamy wersji Pythona %f" % 3.9)  # Używamy wersji Pythona 3.900000
+print("Używamy wersji Pythona %.2f" % 3.9)  # Używamy wersji Pythona 3.90
+print("Używamy wersji Pythona %.1f" % 3.9)  # Używamy wersji Pythona 3.9
+print("Używamy wersji Pythona %.0f" % 3.9)  # Używamy wersji Pythona 4 - zaokrągli przy wyświetleniu
+print("Używamy wersji Pythona %.f" % 3.9)  # Używamy wersji Pythona 4 - zaokrągli przy wyświetleniu
+
+x = 3.99
+print("Liczba: %.f" % x)  # Liczba: 4
+print("Liczba się nie zmieniła:", x)  # Liczba się nie zmieniła: 3.99
+
+# zaokrąglenie wartości liczby
+x = round(x)
+print("Liczba po zaokrągleniu:", x)  # Liczba po zaokrągleniu: 4
+
+x = 3.14157890
+x = round(x, 2)
+print("liczba zaokrąglona do dwóch miejsc po przecinku:", x)
+# liczba zaokrąglona do dwóch miejsc po przecinku: 3.14
+
+print(f"Używamy wersji pythona {wersja}")  # Używamy wersji pythona 3.900001
+print(f"Używamy wersji pythona {wersja:.1f}")  # Używamy wersji pythona 3.9
+print(f"Używamy wersji pythona {wersja:.2f}")  # Używamy wersji pythona 3.90
+print(f"Używamy wersji pythona {wersja:.0f}")  # Używamy wersji pythona 4
+# print(f"Używamy wersji pythona {wersja:.f}")  # ValueError: Format specifier missing precision
