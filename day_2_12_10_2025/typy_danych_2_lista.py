27879267d6ef149a3635dc04b90338d12b955ede
radoslawjaniak <rajkokret660@gmail.com>
2025-10-12 09:29:41 +0200

    [2 3] update typy_danych_2_lista.py

--- day_2_12_10_2025/typy_danych_2_lista.py
+++ day_2_12_10_2025/typy_danych_2_lista.py
@@ -2,4 +2,16 @@
 
 # lista - kolekcja przechowująca dowolną ilość danych, dowolnego typu na raz
 # może w jedej liście przechowywać np.: str, int, bytes...
-# lista zachowuje kolejnosc przy dodawaniu elementów
+# lista zachowuje kolejnosc przy dodawaniu elementów, trafia na koniec listy
+
+# tworzenie pustej listy
+lista = []
+print(lista)  # []
+print(type(lista))  # <class 'list'>
+print(bool(lista))  # False
+
+pusta_lista = list()
+print(pusta_lista)  # []
+print(type(pusta_lista))  # <class 'list'>
+
+#
