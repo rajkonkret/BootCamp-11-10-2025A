1e2493e682876b722c0c5c1105dacd715f42fb42
radoslawjaniak <rajkokret660@gmail.com>
2025-10-12 09:25:39 +0200

    [2 2] add typy_danych_2_lista.py

---
+++ day_2_12_10_2025/typy_danych_2_lista.py
@@ -0,0 +1,5 @@
+# Kolekcje
+
+# lista - kolekcja przechowująca dowolną ilość danych, dowolnego typu na raz
+# może w jedej liście przechowywać np.: str, int, bytes...
+# lista zachowuje kolejnosc przy dodawaniu elementów
