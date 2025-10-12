20e222b75fe8997b25148a2b45af059fa421cdf9
radoslawjaniak <rajkokret660@gmail.com>
2025-10-12 12:49:55 +0200

    [2 36] update typy_danych_2_lista.py

---
+++ day_2_12_10_2025/typy_danych_3_krotka.py
@@ -0,0 +1,35 @@
+# krotka (tupla) - kolekcja do odczytu, niemutowalna
+# lepsze wykorzystanie pamięci
+# przechowywanie konfiguracji
+# jednoelemntowa krotka często jest traktowana jako stała - wypełnia zmnamiona zmiennej
+
+tupla1 = "Radek"
+print(type(tupla1))  # <class 'str'>
+
+tupla2 = ("Radek")
+print(type(tupla2))  # <class 'str'>
+
+# PEP8 zaleca tworzenie tupli jedoelementowej z nawiasami ()
+# czytelnijszy zapis
+tupla3 = ("Radek",)
+print(type(tupla3))  # <class 'tuple'>
+print(tupla3)  # ('Radek',)
+
+tupla4 = "Radek",
+print(type(tupla4))  # <class 'tuple'>
+print(tupla4)  # ('Radek',)
+
+tupla_names = "Radek", "tomek", "Zenek", "Bartek"
+print(type(tupla_names))  # <class 'tuple'>
+print(tupla_names)  # ('Radek', 'tomek', 'Zenek', 'Bartek')
+
+temp = 36, 6
+print(type(temp))  # <class 'tuple'>
+print(temp)  # (36, 6)
+
+# tupla_liczby = 43, 55, 22.34, 11, 200
+tupla_liczby = (43, 55, 22.34, 11, 200)
+print(tupla_liczby)
+print(type(tupla_liczby))
+# (43, 55, 22.34, 11, 200)
+# <class 'tuple'>
