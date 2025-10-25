928193f369de4a76a60fca1fdaed771c63188e4f
radoslawjaniak <rajkokret660@gmail.com>
2025-10-25 10:02:45 +0200

    [3 2] update typy_danych_5_slownik.py

--- day_3_25_10_2025/typy_danych_5_slownik.py
+++ day_3_25_10_2025/typy_danych_5_slownik.py
@@ -8,3 +8,25 @@ my_dict = {"A": "one", "B": "two", "C": "three", "D": "four"}
 print(my_dict)  # {'A': 'one', 'B': 'two', 'C': 'three', 'D': 'four'}
 # ctrl alt l - formatowanie wg PEP8
 
+# tworzenie pustego słownika
+empty_dict = dict()
+print(type(empty_dict))  # sprawdzenie typu, <class 'dict'>
+print(empty_dict)  # {} - pusty słownik
+
+empty_dict_2 = {}
+print(type(empty_dict_2))  # <class 'dict'>
+print(empty_dict_2)  # {} - pusty słownik
+
+data_with_integer = {1: "one", 2: "two", 3: "three"}
+print(data_with_integer)  # {1: 'one', 2: 'two', 3: 'three'}
+
+# klucze mieszane
+dict_mixed = {1: "one", 2: "two", "A": "three"}
+print(dict_mixed)  # {1: 'one', 2: 'two', 'A': 'three'}
+
+# klucz, wartość, para
+print(dict_mixed.keys())  # dict_keys([1, 2, 'A']) # - klucze
+print(dict_mixed.values())  # dict_values(['one', 'two', 'three']) - wartości
+print(dict_mixed.items())  # dict_items([(1, 'one'), (2, 'two'), ('A', 'three')]) - pary
+
+print(data_with_integer.keys())  # dict_keys([1, 2, 3]) - lista kluczy
