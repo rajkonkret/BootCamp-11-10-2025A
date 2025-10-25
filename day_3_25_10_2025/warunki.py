303250c85a1885070cebc922f9c90cab4ea21250
radoslawjaniak <rajkokret660@gmail.com>
2025-10-25 12:54:08 +0200

    [3 18] add warunki.py

--- day_3_25_10_2025/warunki.py
+++ day_3_25_10_2025/warunki.py
@@ -28,3 +28,33 @@ print("Dalsza część programu")
 # Brawo
 # Brawo
 # Dalsza część programu
+
+odp = "Radek"
+print(bool(odp))  # True
+
+if odp:
+    print("OK")  # OK
+
+odp = "Tomek"
+if odp == "Radek":
+    print("Radek")
+else:  # w przeciwnym wypadku, default
+    print("Inny pacjent")
+# Inny pacjent
+
+
+podatek = 0.9
+zarobki = int(input("Podaj dochód"))
+# jeden spełniony warunek - wychodzi z drzewka
+# koljnośc ma znaczenie
+if zarobki < 10_000:
+    podatek = 0.0
+elif zarobki < 40_000:
+    podatek = 0.2
+elif zarobki < 100_000:
+    podatek = 0.4
+else:
+    podatek = 0.9
+
+print(f"Płacisz {zarobki * podatek} pln podatku.")
+# 0.2 dla przedziału 10_000 do 39_999
