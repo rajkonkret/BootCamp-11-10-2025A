ae53f202c51c136fe2ca661f7fcc8cf00ecb7ab1
radoslawjaniak <rajkokret660@gmail.com>
2025-10-25 12:36:52 +0200

    [3 17] add warunki.py

---
+++ day_3_25_10_2025/warunki.py
@@ -0,0 +1,30 @@
+# instrukcje warunkowe
+# instrukcje sterowania przepływem programu
+# if - sterowanie warunkiem
+# if warunek:
+#   komenda(blok) - wykonan gdy warunek spełniony
+
+odp = True
+print(bool(odp))
+# odp = False
+# debuger - pozwala wykonac prokram krok po kroku
+# daje możliwość weryfikacji wartośći zmiennych i parametrów
+# pułąpka - program zatrzyma się tutaj w trybie debug
+if odp:
+    # blok wykonywany gdy warunek spełniony
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+print("Dalsza część programu")
+# True
+# Brawo
+# Brawo
+# Brawo
+# Brawo
+# Brawo
+# Brawo
+# Dalsza część programu
