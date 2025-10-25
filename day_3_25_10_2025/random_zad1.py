8845778632ce94be51497e8f22b90c8d958cc87b
radoslawjaniak <rajkokret660@gmail.com>
2025-10-25 15:01:13 +0200

    [3 27] update random_zad1.py

--- day_3_25_10_2025/random_zad1.py
+++ day_3_25_10_2025/random_zad1.py
@@ -10,3 +10,16 @@ print(random.randrange(5))  # od 0 do 4
 
 print(random.random())  # 0.04727211807938381, float, od 0 do 0.9999999
 print(random.random() * 8)  # 1.9207021073560595, float, od 0 do 7.9999999
+
+lista = [5, 7, 45, 34, 56]
+
+index = random.randrange(len(lista))
+print(index, lista[index])  # 3 34
+
+print(random.choice(lista))  # losuje element z listy: 45
+
+print("-" * 20)
+# bęben maszyby losującej -> lista
+# losowanie kul -> random.choice
+# usunięcei kuli z bębna -> remove
+# wypisanie liczby -> print
