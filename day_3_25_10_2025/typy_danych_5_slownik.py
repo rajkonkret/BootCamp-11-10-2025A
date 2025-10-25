72185f3ffa85d607ca25b1afe61872fb123ee3a4
radoslawjaniak <rajkokret660@gmail.com>
2025-10-25 09:41:57 +0200

    [3 1] add typy_danych_5_slownik.py

---
+++ day_3_25_10_2025/typy_danych_5_slownik.py
@@ -0,0 +1,9 @@
+# słownik - para klucz - wartość
+# {'klucz' : 'wartosc'}
+# ctrl / - szybki komentarz
+# odpowiednik jsona - {"name":"John", "age":30, "car":null}
+# klucze w słowniku  nie mogą się powtarzać
+
+my_dict = {"A": "one", "B": "two", "C": "three", "D": "four"}
+print(my_dict)  # {'A': 'one', 'B': 'two', 'C': 'three', 'D': 'four'}
+# ctrl alt l - formatowanie wg PEP8
