ca4c524c2cae9ef6cf894b52e24e542bdd64a37e
radoslawjaniak <rajkokret660@gmail.com>
2025-10-25 10:47:42 +0200

    [3 7] update typy_danych_5_slownik.py

--- day_3_25_10_2025/typy_danych_5_slownik.py
+++ day_3_25_10_2025/typy_danych_5_slownik.py
@@ -84,3 +84,32 @@ value = [10, 20, 30]
 my_dict_4 = dict.fromkeys(keys, value)
 print(my_dict_4)
 # {'d': [10, 20, 30], 'b': [10, 20, 30], 'a': [10, 20, 30], 'c': [10, 20, 30]}
+
+# zastosowanie fromkeys() do usunięcia duplikatów z listy, bez zmiany kolejności
+# od python 3.7 kolejnośc w słowniku jest zachowana
+keys = [1, 2, 2, 3, 4, 4, 5]
+dict_unique = dict.fromkeys(keys)
+print(dict_unique)  # {1: None, 2: None, 3: None, 4: None, 5: None}
+list_unique = list(dict_unique)
+print(list_unique)  # [1, 2, 3, 4, 5], bez zmainy kolejności
+
+print(list(dict.fromkeys(keys)))  # [1, 2, 3, 4, 5]
+
+# wypisanie wartościa dla klucza
+print(dict_with_dict["A"])  # ['asif', 'jhon', 'maria']
+
+print(my_dict)  # {'A': 'one', 'B': 'two', 'C': 'three', 'D': 'four'}
+print(my_dict['A'])  # one
+
+print(data_with_integer)  # {1: 'one', 2: 'two', 3: 'three'}
+print(data_with_integer[1])  # one
+print(dict_with_all["C"])  # {'age', 10, 'Name'}
+
+# print(my_dict_4['e'])  # KeyError: 'e'
+
+print(my_dict_4.get('a'))  # [10, 20, 30]
+print(my_dict_4.get('e'))  # None, oznacza brak klucza w słowniku
+# None - odpowiednik nulla w innych systemach
+# mozemy ustawić wartośc domyślną, jaka będzie zwracana gdy nie ma klucza w słowniku
+print(my_dict_4.get('e', "Nie ma"))  # Nie ma - gdy brak klucza
+print(my_dict_4.get('a', "Nie ma"))  # [10, 20, 30]
