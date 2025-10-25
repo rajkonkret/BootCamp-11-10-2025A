b49be8a9e12ac45eac52a2a7073108e47c7f8c02
radoslawjaniak <rajkokret660@gmail.com>
2025-10-25 13:40:56 +0200

    [3 19] add warunki.py

--- day_3_25_10_2025/warunki.py
+++ day_3_25_10_2025/warunki.py
@@ -43,18 +43,30 @@ else:  # w przeciwnym wypadku, default
 # Inny pacjent
 
 
-podatek = 0.9
-zarobki = int(input("Podaj dochód"))
-# jeden spełniony warunek - wychodzi z drzewka
-# koljnośc ma znaczenie
-if zarobki < 10_000:
-    podatek = 0.0
-elif zarobki < 40_000:
-    podatek = 0.2
-elif zarobki < 100_000:
-    podatek = 0.4
-else:
-    podatek = 0.9
+# podatek = 0.9
+# zarobki = int(input("Podaj dochód"))
+# # jeden spełniony warunek - wychodzi z drzewka
+# # koljnośc ma znaczenie
+# if zarobki < 10_000:
+#     podatek = 0.0
+# elif zarobki < 40_000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+#
+# print(f"Płacisz {zarobki * podatek} pln podatku.")
+# # 0.2 dla przedziału 10_000 do 39_999
 
-print(f"Płacisz {zarobki * podatek} pln podatku.")
-# 0.2 dla przedziału 10_000 do 39_999
+suma_zam = 250
+if suma_zam > 150:
+    rabacik = 25
+else:
+    rabacik = 0
+
+print(f"Rabat wynosi: {rabacik}")  # Rabat wynosi: 25
+
+# operator warunkowy
+rabat = 25 if suma_zam > 150 else 0
+print(f"Rabat wynosi: {rabat}")  # Rabat wynosi: 25
