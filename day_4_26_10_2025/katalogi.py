95f5b002497684ff18abeefa1ff03faf03a64c4f
radoslawjaniak <rajkokret660@gmail.com>
2025-10-26 13:48:26 +0100

    [4 32] add katalogi.py

---
+++ day_4_26_10_2025/katalogi.py
@@ -0,0 +1,9 @@
+import shutil
+from pathlib import Path
+
+base_path = Path('ops_example')
+base_path2 = Path('ops_example/D')
+
+if base_path.exists() and base_path.is_dir():
+    # Recursively delete a directory tree
+    shutil.rmtree(base_path)
