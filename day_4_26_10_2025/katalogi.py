37c1e527e58001d616473eff0d50c64d3625e182
radoslawjaniak <rajkokret660@gmail.com>
2025-10-26 13:49:45 +0100

    [4 33] add katalogi.py

--- day_4_26_10_2025/katalogi.py
+++ day_4_26_10_2025/katalogi.py
@@ -7,3 +7,10 @@ base_path2 = Path('ops_example/D')
 if base_path.exists() and base_path.is_dir():
     # Recursively delete a directory tree
     shutil.rmtree(base_path)
+
+# tworzenie katalogu
+base_path.mkdir()
+
+path_b = base_path / "A" / "B"
+path_c = base_path / "A" / "C"
+path_d = base_path / "A" / "D"
