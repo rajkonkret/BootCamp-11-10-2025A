6dd8672df5c6961ab394d1b32494f8283d81f894
radoslawjaniak <rajkokret660@gmail.com>
2025-10-26 13:23:02 +0100

    [4 26] add json_zad1

---
+++ day_4_26_10_2025/json_zad1.py
@@ -0,0 +1,8 @@
+# json - typ danych para klucz-wartość
+# {"name":"John", "age":30, "car":null}
+# podwójne cudzysłowia
+# None -> null
+# wykorzystywany w plikach konfiguracyjnych
+# wykorzystywany do komunikacji między systemami
+# odpowiednikiem jsona w pythonie jest słownik
+import json
