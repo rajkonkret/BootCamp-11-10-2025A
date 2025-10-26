0393b289db03efe4a3af7bc6f68cc11959d39cea
radoslawjaniak <rajkokret660@gmail.com>
2025-10-26 09:39:10 +0100

    [4 9] update petle_zad2.py

--- day_4_26_10_2025/petle_zad3.py
+++ day_4_26_10_2025/petle_zad3.py
@@ -17,3 +17,18 @@ licznik = 0
 while licznik < 15:
     licznik += 1
     print("Komunikat 3 !!!")
+
+password = input("Podaj hasło")
+while password != 'secret':
+    password = input("Błędne hasło. Podaj ponownie")
+print("Hasło prawidłowe")
+# Podaj hasłoassasa
+# Błędne hasło. Podaj ponownieasasa
+# Błędne hasło. Podaj ponowniewer
+# Błędne hasło. Podaj ponownienmbm
+# Błędne hasło. Podaj ponowniedvxcvc
+# Błędne hasło. Podaj ponowniezcxvz
+# Błędne hasło. Podaj ponowniesecret
+# Hasło prawidłowe
+
+
