3ad79998157c262e83f2c5a1ce719c27154ceb7f
radoslawjaniak <rajkokret660@gmail.com>
2025-10-26 13:28:15 +0100

    [4 27] update json_zad1

--- day_4_26_10_2025/json_zad1.py
+++ day_4_26_10_2025/json_zad1.py
@@ -6,3 +6,29 @@
 # wykorzystywany do komunikacji między systemami
 # odpowiednikiem jsona w pythonie jest słownik
 import json
+
+person_dict = {'name': 'Radek', 'age': 40, 'czy_pali': None}
+print(person_dict)  # <class 'dict'>
+print(type(person_dict))  # {'name': 'Radek', 'age': 40, 'czy_pali': None}
+
+with open('nasze_dane.json', "w") as f:
+    json.dump(person_dict, f)
+# {"name": "Radek", "age": 40, "czy_pali": null}
+
+# beautify - upiiększanie
+with open('nasze_dane_b.json', "w") as f:
+    json.dump(person_dict, f, indent=4)
+# {
+#     "name": "Radek",
+#     "age": 40,
+#     "czy_pali": null
+# }
+
+# posortowanie po kluczach
+with open('nasze_dane_sort.json', "w") as f:
+    json.dump(person_dict, f, indent=4, sort_keys=True)
+# {
+#     "age": 40,
+#     "czy_pali": null,
+#     "name": "Radek"
+# }
