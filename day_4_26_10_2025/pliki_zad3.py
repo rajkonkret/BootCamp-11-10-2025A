a06358fba16b6938948ef1627f2ea7486538d439
radoslawjaniak <rajkokret660@gmail.com>
2025-10-26 14:18:59 +0100

    [4 41] update pliki_zad3.py

--- day_4_26_10_2025/pliki_zad3.py
+++ day_4_26_10_2025/pliki_zad3.py
@@ -11,3 +11,19 @@ if not os.path.exists(root_dir):
 source_path = os.path.join(root_dir, target_name)
 with open(source_path, "w", encoding="utf-8") as src:
     src.write('Treść\n')
+
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+
+output_path = os.path.join(dest_dir, target_name)
+
+with open(output_path, "w", encoding="utf-8") as out:
+    for dirpath, _, filenames in os.walk(root_dir):
+        for fname in filenames:
+            if fname == target_name:
+                src_path = os.path.join(dirpath, fname)
+                out.write(f"\n Plik z : {src_path}\n")
+                with open(src_path, encoding='utf-8') as f:
+                    shutil.copyfileobj(f, out)
+
+print("Połączone pliki:", output_path)
