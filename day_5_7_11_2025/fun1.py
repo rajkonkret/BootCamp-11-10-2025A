a7aacb9523581b6f4b86bc1a57372670e2ccbfad
radoslawjaniak <rajkokret660@gmail.com>
2025-11-08 09:19:14 +0100

    [5 4] add fun1.py

---
+++ day_5_7_11_2025/fun1.py
@@ -0,0 +1,33 @@
+# funkcje - wydzielony fragment kodu, możemy wykonac w dowolnym momencie
+# funkcja przed użyciem musi zostac zdefiniowana
+# w miejscu definicji funkcja się nie wykona
+# aby funkcja się uruchomiła musimy ją wywołąć
+
+# zmienne globalne
+# dostępne wewnątrz funkcji
+a = 6
+b = 8
+
+
+# deklaracja funkcji
+# PEP8 zaleca by funkcja była oddzielona dwoma liniami od reszty progrmau
+def dodaj():  # funkcja bezargumentowa
+    print(a + b)
+    c = 7  # zmienna loklna, widocznośc tylko wewnatrz funkcji
+
+
+# dwa obowiązkowe argumenty
+def dodaj2(a, b):
+    # a i b o zasięgu lokalanym
+    print(a + b)
+
+
+# wywołanie funkcji
+print(dodaj)  # <function dodaj at 0x105ae19e0>
+print(type(dodaj))  # <class 'function'>
+# nazwa funkcji i () - uruchomienie funkcji
+dodaj()  # 14
+# print(c)
+
+# dodaj2()  # TypeError: dodaj2() missing 2 required positional arguments: 'a' and 'b'
+dodaj2(5, 9)  # 14, obowiązkowo dwa parametry do przekazania
