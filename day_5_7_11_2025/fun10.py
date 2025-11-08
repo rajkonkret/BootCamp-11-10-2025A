ce7cde5e088e4d3efa1630e5b8341bf44f694d91
radoslawjaniak <rajkokret660@gmail.com>
2025-11-08 14:41:32 +0100

    [5 32] update fun10_

--- day_5_7_11_2025/fun10.py
+++ day_5_7_11_2025/fun10.py
@@ -15,3 +15,12 @@ print(f"Reduce [sum_all]: {sum_all}")  # Reduce [sum_all]: 6 -> (1 + 2) +3) = 6
 
 product = reduce(lambda a, b: a * b, [1, 2, 3, 4])
 print(f"Reduce [product]: {product}")  # Reduce [product]: 24
+
+lista = [1, 2, 3, 45, 67, 78, 100, 200, 300]
+sum_all = reduce(lambda a, b: a + b, list(map(lambda n: n * 2, list(filter(lambda n: n % 2 == 0, lista)))))
+print("Reduce:", sum_all)  # Reduce: 1360
+
+l_1 = [i for i in lista if i % 2 == 0]
+l_2 = [i * 2 for i in l_1]
+sum_all = reduce(lambda a, b: a + b, l_2)
+print(sum_all)  # 1360
