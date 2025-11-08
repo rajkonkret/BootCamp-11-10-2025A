f2d4175d2637f4d5cc68c5f03384f1260cf037e8
radoslawjaniak <rajkokret660@gmail.com>
2025-11-08 14:35:57 +0100

    [5 31] update fun10_

--- day_5_7_11_2025/fun10.py
+++ day_5_7_11_2025/fun10.py
@@ -10,4 +10,8 @@ def add(a, b):
 sum_all = reduce(add, [1, 2, 3])
 print(f"Reduce [sum_all]: {sum_all}")  # Reduce [sum_all]: 6
 
+sum_all = reduce(lambda a, b: a + b, [1, 2, 3])
+print(f"Reduce [sum_all]: {sum_all}")  # Reduce [sum_all]: 6 -> (1 + 2) +3) = 6
 
+product = reduce(lambda a, b: a * b, [1, 2, 3, 4])
+print(f"Reduce [product]: {product}")  # Reduce [product]: 24
