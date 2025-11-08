674587279e8ab074e8ad0b07ab40ee08fed6c448
radoslawjaniak <rajkokret660@gmail.com>
2025-11-08 13:13:15 +0100

    [5 18] upate fun8.py

--- day_5_7_11_2025/fun8.py
+++ day_5_7_11_2025/fun8.py
@@ -17,3 +17,25 @@ allparams(1, 2, c=90)
 # {}
 
 allparams(1, 2, c=8, a=90)  # {'a': 90}
+
+
+def allparams_all(a, b, /, c=43, *args, d=256, **kwargs):
+    print(f"{a=}, {b=}")
+    print(f"{c=}, {d=}")
+    print(f'{args}')
+    print(f'{kwargs}')
+
+
+allparams_all(1, 2)  # a=1, b=2
+allparams_all(1, 2, 3)  # c=3,
+allparams_all(1, 2, c=3)  # c=3,
+# c musi byc pozycyjnie, jeśli chce przekazac d
+allparams_all(1, 2, 3, 5)  # (5,)
+allparams_all(1, 2, 3, 5, 6, 7, 8, 11, 12, 14, 15, 100)  # (5, 6, 7, 8, 11, 12, 14, 15, 100)
+# d musimy przekazac po nazwie bo jest po *args
+allparams_all(1, 2, 3, 5, 6, 7, 8, 11, 12, 14, 15, 100, d=89)  # (5, 6, 7, 8, 11, 12, 14, 15, 100)
+allparams_all(1, 2, 3, 5, 6, 7, 8, 11, 12, 14, 15, 100, d=89, e=1000, name="Radek")  # (5, 6, 7, 8, 11, 12, 14, 15, 100)
+# {'e': 1000, 'name': 'Radek'}
+# a trafi do kwargs
+allparams_all(1, 2, 3, 5, 6, 7, 8, 11, 12, 14, 15, 100, d=89, e=1000, name="Radek",
+              a=10)  # (5, 6, 7, 8, 11, 12, 14, 15, 100)
