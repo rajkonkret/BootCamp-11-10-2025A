e2ebc1659e103e3634c870a15cc8a3e792ba642b
radoslawjaniak <rajkokret660@gmail.com>
2025-11-09 14:47:13 +0100

    [6 21] add kl6

---
+++ day_6_9_11_2025/kl6.py
@@ -0,0 +1,9 @@
+# stworzyc kalse Pracownik
+# imie, nazwisko, pensja
+# przedstaw_sie(), oblicz_pensje()
+# zrobic kalse Manager dziedziczącą po klasie Pracownik
+# zastonowić co może dziedziczyć bez zmiany, co musi nadpisac w klasie Manager
+# dodac dokumentację
+
+# stworzyc jedego pracownika i jedego managera
+# użyc metod z klas
