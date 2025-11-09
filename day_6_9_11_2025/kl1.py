56888cc2925bdd7b525652cd332fb632e5d5d42e
radoslawjaniak <rajkokret660@gmail.com>
2025-11-09 12:36:17 +0100

    [6 7] add kl1.py

---
+++ day_6_9_11_2025/kl1.py
@@ -0,0 +1,7 @@
+# klasa - szablon, przepis, wzor
+# obiekt - zbudowany wg przepiu, instancja klasy
+# pola, funkcje (metody)
+# klasa musi byc najpierw zadeklarowana
+# tworzenie obiektu uruchmia metodę __init__ (konstruktor)
+# paradygmaty:
+# enkapsulacja, hermetyzacja, polimorfizm, dziedziczenie, abstrakcja
