6fb68e4ac940e285ea9983e2fed0458a684d7576
radoslawjaniak <rajkokret660@gmail.com>
2025-11-09 14:19:12 +0100

    [6 17] update kl4

--- day_6_9_11_2025/kl4.py
+++ day_6_9_11_2025/kl4.py
@@ -1,4 +1,56 @@
 # napisać klasę Dom
 # ma posiadac pola prywatne kolor, liczba_okien, metraz
-# dodac metode do odczytu i zapisy tych pól
+# dodac metode do odczytu i zapisy tych pól (wszystkich) (gettery/settery)
 # dodać metode prywatną __farba() -> zabrakło farby
+# enkapsulacja - pola prywatne i metody do dodczytu i zapisu tych pol
+class Dom:
+    """
+    Klasa opisujaca Dom
+    """
+
+    def __init__(self, metraz, kolor, liczba_okien):
+        self.__metraz = metraz
+        self.__kolor = kolor
+        self.__liczba_okien = liczba_okien
+
+    def wyswietl_okna(self):
+        print(f"Mam {self.__liczba_okien} okna/okien")
+
+    def wyswietl_kolor(self):
+        print(f"Mam kolor: {self.__kolor}")
+
+    def wyswietl_metraz(self):
+        print(f"Mam metraz: {self.__metraz} m2")
+
+    def zmien_okna(self):
+        odp = int(input("Podaj liczbę okien"))
+        self.__liczba_okien = odp
+        self.wyswietl_okna()
+
+    def zmien_metraz(self):
+        odp = int(input("Podaj nowy metraz"))
+        self.__metraz = odp
+        self.wyswietl_metraz()
+
+    def zmien_kolor(self):
+        odp = input("Podaj nowy kolor")
+        self.__kolor = odp
+        self.wyswietl_kolor()
+        self.__farba()
+
+    def __farba(self):
+        print("Zabrakło farby")
+
+
+dom1 = Dom(200, "czerwony", 11)
+dom1.wyswietl_kolor()
+dom1.wyswietl_metraz()
+dom1.wyswietl_okna()
+# Mam kolor: czerwony
+# Mam metraz: 200 m2
+# Mam 11 okna/okien
+
+dom1.zmien_kolor()
+# Podaj nowy kolorbiały
+# Mam kolor: biały
+# Zabrakło farby
