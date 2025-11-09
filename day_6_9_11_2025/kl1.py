9cfe4c153a740475ff8db3156a4d2bc61b852023
radoslawjaniak <rajkokret660@gmail.com>
2025-11-09 12:57:42 +0100

    [6 8] update kl1.py

--- day_6_9_11_2025/kl1.py
+++ day_6_9_11_2025/kl1.py
@@ -5,3 +5,22 @@
 # tworzenie obiektu uruchmia metodę __init__ (konstruktor)
 # paradygmaty:
 # enkapsulacja, hermetyzacja, polimorfizm, dziedziczenie, abstrakcja
+import pydoc
+
+
+# PascalCase -> UpperCamelCase
+# deklaracja klasy
+class Human:
+    """
+    Klasa Human opisująca człowieka w pythonie
+    """
+
+
+print(Human.__doc__)  # Klasa Human opisująca człowieka w pythonie
+print(print.__doc__)
+# cd day_6_9_11_2025/ - wejscie do katalogu
+# pydoc -b - uruchomienie serwera  zdokumentacją
+# python -m pydoc -b
+help("kl1")
+print(pydoc.render_doc("kl1"))
+# pydoc -w kl1 - dokumentacja w postaci pliku html
