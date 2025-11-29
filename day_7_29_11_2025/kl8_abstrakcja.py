6c89b02d502a9d7428eb824894cf0ed7f97f6881
radoslawjaniak <rajkokret660@gmail.com>
2025-11-29 09:29:58 +0100

    [7 7] update kl8

--- day_7_29_11_2025/kl8_abstrakcja.py
+++ day_7_29_11_2025/kl8_abstrakcja.py
@@ -2,8 +2,10 @@
 # klasa z której nie możemy zrobic obiektu
 # posiada metode abstrakcyjną
 # metoda abstrakcyjna - @abstractmethod
+from abc import ABC, abstractmethod
 
-class Ptak:
+
+class Ptak(ABC):
     """
     Klasa opisująca ptaka w Pythonie
     """
@@ -15,6 +17,7 @@ class Ptak:
     def latam(self):
         print("Tu", self.gatunek, 'Lecę z szybkością', self.szybkosc)
 
+    @abstractmethod # metoda abstrakcyjna
     def wydaj_odglos(self):
         pass
 
@@ -30,11 +33,36 @@ class Kura(Ptak):
         print("Ko ko ko ko ko")
 
 
-or1 = Ptak("Orzeł", 45)
-or1.latam()  # tu Orzeł Lecę z szybkością 45
+class Orzel(Ptak):
+    """
+    Klasa Orzel dziedziczy po klasie Ptak
+    """
 
-kur1 = Ptak("Kura", 0)
-kur1.latam()  # tu Kura Lecę z szybkością 0
+    def wydaj_odglos(self):
+        print("kier kir kir kier")
+
+# TypeError: Can't instantiate abstract class Sowa without an implementation
+# for abstract method 'wydaj_odglos'
+class Sowa(Ptak):
+    """
+    Kalsa Sowa
+    """
+
+
+# TypeError: Can't instantiate abstract class Ptak
+# without an implementation for abstract method 'wydaj_odglos'
+# or1 = Ptak("Orzeł", 45)
+# or1.latam()  # tu Orzeł Lecę z szybkością 45
+#
+# kur1 = Ptak("Kura", 0)
+# kur1.latam()  # tu Kura Lecę z szybkością 0
 
 kur2 = Kura("Kura złocista")
 kur2.latam()  # Tu Kura złocista Ja nie latam
+
+or2 = Orzel('Bielik', 50)
+or2.latam()  # Tu Bielik Lecę z szybkością 50
+
+# TypeError: Can't instantiate abstract class Sowa
+# without an implementation for abstract method 'wydaj_odglos'
+# sowa1 = Sowa("Sowa", 25)
