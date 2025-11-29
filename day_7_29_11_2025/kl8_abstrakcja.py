6f056beb32c83e8cc80c19404a0622b6a029f821
radoslawjaniak <rajkokret660@gmail.com>
2025-11-29 09:17:27 +0100

    [7 6] update kl8

--- day_7_29_11_2025/kl8_abstrakcja.py
+++ day_7_29_11_2025/kl8_abstrakcja.py
@@ -13,4 +13,28 @@ class Ptak:
         self.szybkosc = szybkosc
 
     def latam(self):
-        print("tu", self.gatunek, 'Lecę z szybkością', self.szybkosc)
+        print("Tu", self.gatunek, 'Lecę z szybkością', self.szybkosc)
+
+    def wydaj_odglos(self):
+        pass
+
+
+class Kura(Ptak):
+    def __init__(self, gatunek):
+        super().__init__(gatunek, 0)
+
+    def latam(self):
+        print("Tu", self.gatunek, "Ja nie latam")
+
+    def wydaj_odglos(self):
+        print("Ko ko ko ko ko")
+
+
+or1 = Ptak("Orzeł", 45)
+or1.latam()  # tu Orzeł Lecę z szybkością 45
+
+kur1 = Ptak("Kura", 0)
+kur1.latam()  # tu Kura Lecę z szybkością 0
+
+kur2 = Kura("Kura złocista")
+kur2.latam()  # Tu Kura złocista Ja nie latam
