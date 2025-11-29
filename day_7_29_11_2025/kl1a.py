ab3279dc61d5062dc8d6bb376b34d1e664fba2c1
radoslawjaniak <rajkokret660@gmail.com>
2025-11-29 10:22:32 +0100

    [7 10] update kl1a

--- day_7_29_11_2025/kl1a.py
+++ day_7_29_11_2025/kl1a.py
@@ -26,6 +26,18 @@ class MyFirstClass:
         self.x = x
         self.y = y
 
+    def reset(self):
+        self.move(0, 0)
+
+    def calculate(self, other: "MyFirstClass") -> float:
+        """
+        Metoda zwraca odległość punktów w przestrzeni euklidesowej
+        sqrt(x*x + y*y)
+        :param other:
+        :return:
+        """
+        return math.hypot(self.x - other.x, self.y - other.y)
+
     # metody tostring
     def __str__(self):
         return f"({self.x, self.y})"
@@ -39,3 +51,11 @@ print(ob.y)
 
 ob.move(67, 90)
 print(ob)  # ((67, 90))
+ob.reset()
+print(ob)  # ((0, 0))
+ob_str = str(ob)
+lista = [ob_str]
+print(lista)  # ['((0, 0))']
+
+ob2 = MyFirstClass(7, 98)
+ob3 = MyFirstClass(43, 134)
