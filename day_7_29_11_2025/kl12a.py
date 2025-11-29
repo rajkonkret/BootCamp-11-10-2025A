e49ba8fefbfdc0f25cceb041a263d0fdd0612333
radoslawjaniak <rajkokret660@gmail.com>
2025-11-29 14:41:54 +0100

    [7 31] update kl12a

--- day_7_29_11_2025/kl12a.py
+++ day_7_29_11_2025/kl12a.py
@@ -34,3 +34,17 @@ class KalkulatorTemperatur:
     @staticmethod
     def fahrenheit_to_celcius(farenheit):
         return (farenheit - 32) * 5 / 9
+
+
+print(f"{KalkulatorTemperatur.fahrenheit_to_celcius(100):.2f}")  # 37.78
+print(f"{KalkulatorTemperatur.fahrenheit_to_celcius(2):.2f}")  # -16.67
+
+print(f"{KalkulatorTemperatur.celcius_to_farenheit(37.78):.2f}")  # 100.00
+
+assert 100.0 == KalkulatorTemperatur.celcius_to_farenheit(37.78)
+# assert 100.0 == KalkulatorTemperatur.celcius_to_farenheit(37.7777777778)
+# Traceback (most recent call last):
+#   File "/Users/radoslawjaniak/PycharmProjects/BootCamp-11-10-2025A/day_7_29_11_2025/kl12a.py", line 44, in <module>
+#     assert 100.0 == KalkulatorTemperatur.celcius_to_farenheit(37.7777777778)
+#            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# AssertionError
