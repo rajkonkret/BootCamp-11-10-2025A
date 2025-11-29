372dc262f0a8eab513af2afe2ef5f27b80532532
radoslawjaniak <rajkokret660@gmail.com>
2025-11-29 14:24:06 +0100

    [7 29] update kl11a

--- day_7_29_11_2025/kl11a.py
+++ day_7_29_11_2025/kl11a.py
@@ -12,3 +12,33 @@ print(MyClassCounter.increment_counter())
 print(MyClassCounter.increment_counter())
 c = MyClassCounter()
 print(c.increment_counter())  # 4
+
+
+class Osoba:
+    def __init__(self, imie, nazwisko):
+        self.imie = imie
+        self.nazwisko = nazwisko
+
+    @classmethod
+    def from_full_name(cls, full_name):
+        imie, nazwisko = full_name.split()
+        return cls(imie, nazwisko)
+
+
+osoba1 = Osoba("Jan", "Kowalski")
+print(osoba1.imie, osoba1.nazwisko)
+# Jan Kowalski
+
+dane = "Jan Kowalski"
+print(dane.split())  # ['Jan', 'Kowalski']
+imie, nazwisko = dane.split()
+print(imie)
+print(nazwisko)
+osoba2 = Osoba(imie, nazwisko)
+print(osoba2.imie, osoba2.nazwisko)  # Jan Kowalski
+
+osoba3 = Osoba.from_full_name(dane)
+print(osoba3.imie)
+print(osoba3.nazwisko)
+# Jan
+# Kowalski
