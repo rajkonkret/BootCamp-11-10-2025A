eee8ade4a294bf6facf102993ff3835ab5ccd8bf
radoslawjaniak <rajkokret660@gmail.com>
2025-11-30 11:23:55 +0100

    [8 29] update kl19a.py

--- day_8_30_11_2025/kl19a.py
+++ day_8_30_11_2025/kl19a.py
@@ -8,3 +8,12 @@ class APIError(Exception):
         msg = response.get('error', "unknown error")
         code = response.get("code", 500)
         return APIError(msg, code=code)
+
+
+# symulujemy odpowiedz z serwera
+response = {"error": "Brak tokena JWT", "code": 401}
+raise APIError.from_response(response)
+# Traceback (most recent call last):
+#   File "/Users/radoslawjaniak/PycharmProjects/BootCamp-11-10-2025A/day_8_30_11_2025/kl19a.py", line 15, in <module>
+#     raise APIError.from_response(response)
+# APIError: Brak tokena JWT
