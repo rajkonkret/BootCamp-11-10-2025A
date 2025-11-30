06148a25c888d0b441a745a02e9f3701dcccc5ad
radoslawjaniak <rajkokret660@gmail.com>
2025-11-30 14:42:26 +0100

    [8 55] update generator_zad2.py

--- day_8_30_11_2025/generator_zad3.py
+++ day_8_30_11_2025/generator_zad3.py
@@ -4,7 +4,19 @@ def count_down(min):
         yield count
         count -= 1
 
+
 def count_up_to(max):
     count = 1
     while count <= max:
         yield
+        count += 1
+
+
+c_u = count_up_to(3)
+c_d = count_down(3)
+
+for n in c_u:
+    print(n)
+
+for n in c_d:
+    print(n)
