c049d5d51deb223d6615547dbab365f2dd521539
radoslawjaniak <rajkokret660@gmail.com>
2025-11-30 11:56:46 +0100

    [8 35] update kl22a_iterator.py

--- day_8_30_11_2025/kl22a_iterator.py
+++ day_8_30_11_2025/kl22a_iterator.py
@@ -69,3 +69,13 @@ class Count:
         """
         self.current = low
         self.high = high
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.high:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current - 1
