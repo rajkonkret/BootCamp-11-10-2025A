d76abe69bb82df2b47227d9100be63ec50b2db09
radoslawjaniak <rajkokret660@gmail.com>
2025-11-30 09:41:55 +0100

    [8 11] update kl14a.py

--- day_8_30_11_2025/kl14a.py
+++ day_8_30_11_2025/kl14a.py
@@ -1,5 +1,28 @@
+# class Person:
+#     def __init__(self, first_name, last_name, id):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.id = id
+#
+#
+# p1 = Person("Jan", "Kowalski", 1)
+# print(p1)  # <__main__.Person object at 0x102c3a270>
+
+from dataclasses import dataclass
+
+
+@dataclass
 class Person:
-    def __init__(self, first_name, last_name, id):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.id = id
+    first_name: str
+    last_name: str
+    id: int
+
+    def greet(self):
+        print(self.last_name)
+
+
+p2 = Person("Jan", 'Kowalski', 1)
+print(p2)  # Person(first_name='Jan', last_name='Kowalski', id=1)
+
+p3 = Person("Maciej", "Nowak", 2)
+print(p3)  # Person(first_name='Maciej', last_name='Nowak', id=2)
