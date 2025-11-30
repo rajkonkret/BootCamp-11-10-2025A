5064c90af6a2a03d21c07b5cc67b843a2ee820c0
radoslawjaniak <rajkokret660@gmail.com>
2025-11-30 14:55:26 +0100

    [8 57] update generator_zad 3.py

--- day_8_30_11_2025/generator_zad3.py
+++ day_8_30_11_2025/generator_zad3.py
@@ -1,3 +1,6 @@
+from types import new_class
+
+
 def count_down(min):
     count = min
     while count > 0:
@@ -15,8 +18,24 @@ def count_up_to(max):
 c_u = count_up_to(3)
 c_d = count_down(3)
 
-for n in c_u:
-    print(n)
 
-for n in c_d:
-    print(n)
+#
+# for n in c_u:
+#     print(n)
+#
+# for n in c_d:
+#     print(n)
+
+def combine(gen1, gen2):
+    yield from gen1  # najpierw generuje z gen1
+    yield from gen2  # następnie będzie generował z gen2
+
+
+c = combine(c_u, c_d)
+
+print(next(c))  # 1
+print(next(c))  # 2
+print(next(c))  # 3
+print(next(c))  # 3
+print(next(c))  # 2
+print(next(c))  # 1
