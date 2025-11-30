acacc12907f22737366583d59286a691f2dec4f2
radoslawjaniak <rajkokret660@gmail.com>
2025-11-30 14:18:37 +0100

    [8 51] update generator_zad2.py

--- day_8_30_11_2025/generator_zad2.py
+++ day_8_30_11_2025/generator_zad2.py
@@ -39,3 +39,34 @@ print(next(g4))
 print(next(g4))
 print(next(g4))
 
+
+def fibo():
+    a, b = 0, 1
+    while True:
+        yield b
+        a, b = b, a + b
+
+
+fib1 = fibo()
+print(next(fib1))  # 1
+print(next(fib1))  # 1
+print(next(fib1))  # 2
+print(next(fib1))  # 3
+print(next(fib1))  # 5
+print(next(fib1))  # 8
+
+
+def fibo_with_index(n):
+    a, b = 0, 1
+    for ind in range(n):
+        yield ind, b
+        a, b = b, a + b
+
+
+fib2 = fibo_with_index(10)
+print(next(fib2))  # (0, 1)
+print(next(fib2))  # (1, 1)
+print(next(fib2))  # (2, 2)
+print(next(fib2))  # (3, 3)
+print(next(fib2))  # (4, 5)
+print(next(fib2))  # (5, 8)
