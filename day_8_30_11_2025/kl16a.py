dd65ba81699770976354fe60cf72af1b520d6151
radoslawjaniak <rajkokret660@gmail.com>
2025-11-30 10:09:55 +0100

    [8 17] update kl16a.py

--- day_8_30_11_2025/kl16a.py
+++ day_8_30_11_2025/kl16a.py
@@ -2,4 +2,11 @@
 # zapisać słownik do pliku i odczytac za pomocą pickle
 # odczytac wartości klucza w słowniku
 
+import pickle
 
+slownik = {"name": "Radek", "age": 78}
+print(type(slownik))  # <class 'dict'>
+print(slownik)  # {'name': 'Radek', 'age': 78}
+
+with open("dickt1.pkl", "wb") as f:
+    pickle.dump(slownik, f)
