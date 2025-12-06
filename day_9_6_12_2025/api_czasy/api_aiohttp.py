9c39bed1e05c0ef1d316aed0dd06dbab7c9ff2af
radoslawjaniak <rajkokret660@gmail.com>
2025-12-06 14:46:28 +0100

    [9 33] update api_czasy.py.py.py

--- day_9_6_12_2025/api_czasy/api_aiohttp.py
+++ day_9_6_12_2025/api_czasy/api_aiohttp.py
@@ -9,3 +9,21 @@ async def fetch(url, session, index):
         print(f"Text: {text}")
 
         return response.status
+
+
+async def measure_aiohttp():
+    url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
+    tasks = []
+
+    # pomiar czasu dla wszystkich zapytań
+    overall_start_time = time.time()
+
+    async with aiohttp.ClientSession() as session:
+        for i in range(100):
+            tasks.append(fetch(url, session, i + 1))
+
+        # przekazanie zadan do asyncio
+        statuses = await asyncio.gather(*tasks)
+
+    overall_elapsed_time = time.time() - overall_start_time
+    print(f'Overall time for 100 requests: {overall_elapsed_time:.4f} seconds.')
