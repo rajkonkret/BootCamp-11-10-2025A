64afd9ad65cde6a027f9556680dd48e825645715
radoslawjaniak <rajkokret660@gmail.com>
2025-12-06 14:25:21 +0100

    [9 26] update api_czasy.py.py.py

--- day_9_6_12_2025/api_czasy/api_nbp_requests.py
+++ day_9_6_12_2025/api_czasy/api_nbp_requests.py
@@ -1,12 +1,12 @@
 import time
 import requests
 
-
 url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
+
 
 def multiple_requests():
     start_time = time.time()
-    for _ in range(1):
+    for _ in range(100):
         r = requests.get(url)
 
     elapsed_time = time.time() - start_time
@@ -14,3 +14,5 @@ def multiple_requests():
 
 
 multiple_requests()
+# Requests time: 0.08314704895019531
+# 100x ->  Requests time: 7.272820949554443
