5c9c8e5f8749bbd88b840a7a7587eb96101dba0d
radoslawjaniak <rajkokret660@gmail.com>
2025-12-06 12:40:41 +0100

    [9 18] update api_randomuser.py.py

--- day_9_6_12_2025/api_randomuser.py
+++ day_9_6_12_2025/api_randomuser.py
@@ -1,1 +1,27 @@
+import requests
+from pydantic import BaseModel
+
 url = "https://randomuser.me/api/"
+
+response = requests.get(url)
+print(response)
+
+print(response.text)
+
+data = response.json()
+
+user = data['results'][0]
+print(user)
+
+print(f"Osoba: {user['name']}")
+# Osoba: {'title': 'Mr', 'first': 'David', 'last': 'Harris'}
+print(f"Imię: {user['name']['first']}")  # Imię: David
+print(f"Nazwisko: {user['name']['last']}")  # Nazwisko: Harris
+
+print(f"Numer telefonu: {user['phone']}")  # Numer telefonu: (664) 429 3469
+
+# "picture": {
+#         "large": "https://randomuser.me/api/portraits/women/3.jpg",
+#         "medium": "https://randomuser.me/api/portraits/med/women/3.jpg",
+#         "thumbnail": "https://randomuser.me/api/portraits/thumb/women/3.jpg"
+#       }
