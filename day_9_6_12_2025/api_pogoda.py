3c5ead5f9ae356e952fe8a1fc4da8eed2028ddf2
radoslawjaniak <rajkokret660@gmail.com>
2025-12-06 12:13:28 +0100

    [9 14] update abi_pogoda.py

--- day_9_6_12_2025/api_pogoda.py
+++ day_9_6_12_2025/api_pogoda.py
@@ -2,8 +2,21 @@ import requests
 
 API_KEY = ""
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid={API_KEY}"
+url = f"https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid={API_KEY}&lang=pl&units=metric&format=json"
 print(url)
 page = requests.get(url)
 print(page)
 print(page.text)
+
+data = page.json()
+print("Miasto:", data['name'])  # Miasto: Warszawa
+print("Miasto:", data['weather'][0]['description'])  # Miasto: zachmurzenie duże
+print("Aktualna temperatura:", data['main']['temp'])
+print("Aktualna temperatura:", data['main']['temp_min'])
+print("Aktualna temperatura:", data['main']['temp_max'])
+# Miasto: zachmurzenie duże
+# Aktualna temperatura: 7.25
+# Aktualna temperatura: 6.47
+# Aktualna temperatura: 8.21
+
+print(50 * "-")
