0c6af1317cd271b9bde7fa669825b11ab5885d1e
radoslawjaniak <rajkokret660@gmail.com>
2025-12-06 14:32:00 +0100

    [9 29] update api_czasy.py.py.py

--- day_9_6_12_2025/api_czasy/api_httpx.py
+++ day_9_6_12_2025/api_czasy/api_httpx.py
@@ -4,9 +4,18 @@ import asyncio
 
 url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
 
+
 async def fetch_data(client, url, index):
     start_time = time.time()
     response = await client.get(url)
     elapsed_time = time.time() - start_time
     print(f"Request: {index}: Status {response.status.code}, Time: {elapsed_time}")
 
+    try:
+        json_data = response.json()
+        print(f"Request {index}: Data: {json_data}")
+    except httpx.HTTPStatusError as e:
+        print(f"Request {index}: Failed with status: {response.status_code}:", e)
+
+async def multiple_httpx():
+    start_time = time.time()
