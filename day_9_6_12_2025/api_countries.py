2bc8da3e416d5726dce33d5691617f80f3672b8f
radoslawjaniak <rajkokret660@gmail.com>
2025-12-06 09:38:37 +0100

    [9 7] update api_countries.py

--- day_9_6_12_2025/api_countries.py
+++ day_9_6_12_2025/api_countries.py
@@ -5,8 +5,8 @@ url = "https://restcountries.com/v3.1/name/Poland"
 response = requests.get(url)
 # print(response.text)
 
-data = response.json() # zamiana na słownik
-print(type(data)) # <class 'list'>
+data = response.json()  # zamiana na słownik
+print(type(data))  # <class 'list'>
 # print(data[0])
 
 country = data[0]
@@ -14,8 +14,13 @@ print(f"Nazwa kraju: {country['name']}")
 # Nazwa kraju: {'common': 'Poland', 'official': 'Republic of Poland',
 # 'nativeName': {'pol': {'official': 'Rzeczpospolita Polska', 'common': 'Polska'}}}
 
-print(f"Nazwa główna: {country['name']['common']}") # Nazwa główna: Poland
-print(f"Nazwa oficjalna: {country['name']['official']}") # Nazwa oficjalna: Republic of Poland
+print(f"Nazwa główna: {country['name']['common']}")  # Nazwa główna: Poland
+print(f"Nazwa oficjalna: {country['name']['official']}")  # Nazwa oficjalna: Republic of Poland
 
 # stolica kraju
 # populacja
+
+print(f"Stolica kraju: {country['capital']}")  # Stolica kraju: ['Warsaw']
+print(f"Stolica kraju: {country['capital'][0]}")  # Stolica kraju: Warsaw
+
+print(f"Liczba ludności: {country['population']}")  # Liczba ludności: 37392000
