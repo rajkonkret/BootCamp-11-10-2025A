21d64b7d530c7ed6152fb5509e965eb9e71ddd12
radoslawjaniak <rajkokret660@gmail.com>
2025-12-06 09:01:40 +0100

    [9 4] update api_chuck.py

--- day_9_6_12_2025/api_chuck.py
+++ day_9_6_12_2025/api_chuck.py
@@ -1,2 +1,16 @@
 # skorzystac z api chuck norris
 # https://api.chucknorris.io/
+
+import requests
+
+url = "https://api.chucknorris.io/jokes/random"
+
+response = requests.get(url)
+
+print(response.text)
+data = response.json()
+
+print(data)
+
+print(data.keys())
+# dict_keys(['categories', 'created_at', 'icon_url', 'id', 'updated_at', 'url', 'value'])
