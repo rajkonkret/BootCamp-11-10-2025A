74a3879a0425c9efda9211a0936c0163c726a748
radoslawjaniak <rajkokret660@gmail.com>
2025-12-07 10:53:55 +0100

    [10 20] watki_zad3.py

---
+++ day_10_7_12_2025/watki_zad3.py
@@ -0,0 +1,15 @@
+import os
+from concurrent.futures import ProcessPoolExecutor
+
+
+# GIL
+
+def worker(i):
+    print(f"Proces: {i} w PID: {os.getpid()}")
+
+
+# powinno byc uruchmiane w formule if main
+if __name__ == '__main__':
+    with ProcessPoolExecutor(max_workers=5) as executor:
+        for i in range(20):
+            executor.submit(worker, i)
