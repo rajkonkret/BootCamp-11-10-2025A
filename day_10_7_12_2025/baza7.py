37946eda488aec83c7e28107cb14ed5bfbfd8839
radoslawjaniak <rajkokret660@gmail.com>
2025-12-07 14:26:59 +0100

    [10 35] update baza7.py

--- day_10_7_12_2025/baza7.py
+++ day_10_7_12_2025/baza7.py
@@ -5,6 +5,10 @@ sql_connection = None
 try:
     # sql_connection = sqlite3.connect(":memory:")  # baza umieszczona w pamięci
     sql_connection = sqlite3.connect("sqlite_python.db")  # baza w pliku
+
+    # Gdybym chciał dane w słowniku
+    sql_connection.row_factory = sqlite3.Row
+
     cursor = sql_connection.cursor()
     print("Baza zostałą podłączona")
 
@@ -31,9 +35,18 @@ try:
     rows = cursor.fetchall()
     for r in rows:
         print(r)
+        print(dict(r))
     # Baza zostałą podłączona
     # (3, 'C#', 10000.0)
     # (4, 'Scala', 5600.0)
+    # Baza zostałą zamknięta
+
+    # Gdy ustawię na dane słownikowe
+    # Baza zostałą podłączona
+    # <sqlite3.Row object at 0x10082fb80>
+    # {'id': 3, 'name': 'C#', 'price': 10000.0}
+    # <sqlite3.Row object at 0x10082fbe0>
+    # {'id': 4, 'name': 'Scala', 'price': 5600.0}
     # Baza zostałą zamknięta
 
 except sqlite3.Error as e:
