b247d3ea8627338f1cff7414822b9f1add8f7bbd
radoslawjaniak <rajkokret660@gmail.com>
2025-12-07 11:01:11 +0100

    [10 21] watki_zad3.py

--- day_10_7_12_2025/watki_zad3.py
+++ day_10_7_12_2025/watki_zad3.py
@@ -8,8 +8,15 @@ def worker(i):
     print(f"Proces: {i} w PID: {os.getpid()}")
 
 
-# powinno byc uruchmiane w formule if main
-if __name__ == '__main__':
+def main():
     with ProcessPoolExecutor(max_workers=5) as executor:
         for i in range(20):
             executor.submit(worker, i)
+
+
+# powinno byc uruchmiane w formule if main
+if __name__ == '__main__':
+    # with ProcessPoolExecutor(max_workers=5) as executor:
+    #     for i in range(20):
+    #         executor.submit(worker, i)
+    main()
