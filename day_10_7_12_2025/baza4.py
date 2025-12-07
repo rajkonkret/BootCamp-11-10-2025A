9c8755ae205da1c41ebc753efdd9e11dac181ae7
radoslawjaniak <rajkokret660@gmail.com>
2025-12-07 13:25:21 +0100

    [10 29] update baza4.py

---
+++ day_10_7_12_2025/baza4.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+sql_connection = None
+
+try:
+    # sql_connection = sqlite3.connect(":memory:")  # baza umieszczona w pamięci
+    sql_connection = sqlite3.connect("sqlite_python.db")  # baza w pliku
+    cursor = sql_connection.cursor()
+    print("Baza zostałą podłączona")
+
+    insert = """
+    INSERT INTO software (id,name,price) VALUES (1, 'Python',100);
+    """
+
+    cursor.execute(insert)
+    sql_connection.commit()
+
+except sqlite3.Error as e:
+    print("Bład bazy danych", e)
+finally:
+    if sql_connection:
+        sql_connection.close()
+        print("Baza zostałą zamknięta")
+
+# Baza zostałą podłączona
+# Baza zostałą zamknięta
