248d08bfed306b97ab71b1b2257e25238ef3b401
radoslawjaniak <rajkokret660@gmail.com>
2025-12-07 08:12:55 +0100

    [10 2] update xml_zad1.py

--- day_10_7_12_2025/xml_zad1.py
+++ day_10_7_12_2025/xml_zad1.py
@@ -2,13 +2,21 @@
 # <quiz></quiz>
 from xml.dom import minidom
 
-root = minidom.Document()
+root = minidom.Document()  # <?xml version="1.0" ?>
 
 xml = root.createElement('root')
-root.appendChild(xml)
+root.appendChild(xml)  # <root>
 
 productChild = root.createElement('product')
 productChild.setAttribute('name', "Python=to-Python")
-xml.appendChild(productChild)
+xml.appendChild(productChild)  # <product name="Python=to-Python"/>
 
-print(root) # <xml.dom.minidom.Document object at 0x105948530>
+print(root)  # <xml.dom.minidom.Document object at 0x105948530>
+
+xmlStr = root.toprettyxml()
+print(xmlStr)
+# <xml.dom.minidom.Document object at 0x10a0dc350>
+# <?xml version="1.0" ?>
+# <root>
+# 	<product name="Python=to-Python"/>
+# </root>
