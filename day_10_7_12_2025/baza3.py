a499f268386f8ddb863d6b3ab3d2ef8968932aef
radoslawjaniak <rajkokret660@gmail.com>
2025-12-07 13:18:29 +0100

    [10 26] update baza3.py.by.py

--- day_10_7_12_2025/baza3.py
+++ day_10_7_12_2025/baza3.py
@@ -7,6 +7,19 @@ try:
     sql_connection = sqlite3.connect("sqlite_python.db")  # baza w pliku
     cursor = sql_connection.cursor()
     print("Baza zostałą podłączona")
+
+    query ='''
+    CREATE TABLE IF NOT EXISTS developers (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    joining_date DATETIME,
+    salary REAL NOT NULL);
+    '''
+
+    cursor.execute(query)
+    sql_connection.commit()
+
 except sqlite3.Error as e:
     print("Bład bazy danych", e)
 finally:
