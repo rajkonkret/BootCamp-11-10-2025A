29841315931cc6a50ef45da21e0234c3e66b2174
radoslawjaniak <rajkokret660@gmail.com>
2025-12-20 12:48:02 +0100

    [11 17] update baza16 context manager

--- day_11_20_12_2025 /baza16_context_manager.py
+++ day_11_20_12_2025 /baza16_context_manager.py
@@ -3,6 +3,7 @@
 # __enter__ - wykonuje się przy uruchomieniu
 # __exit__ - wykonuje się przy wyjsciu(niezależnie czy jest bład czy nie)
 import sqlite3
+from pprint import pprint
 
 
 class SQLiteDBContextManager:
@@ -25,8 +26,41 @@ class SQLiteDBContextManager:
 
 
 db_name = "my_data.db"
+lista = []
 
 with SQLiteDBContextManager(db_name) as conn:
     cursor = conn.cursor()
 
     cursor.execute('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT);')
+    cursor.execute("INSERT INTO users (name) VALUES (?);", ("John",))
+    cursor.execute("INSERT INTO users (name) VALUES (?);", ("Alice",))
+    cursor.execute("INSERT INTO users (name) VALUES (?);", ("Tom",))
+    cursor.execute("INSERT INTO users (name) VALUES (?);", ("Radek",))
+
+    # select = cursor.execute("SELECT * FROM  users;")
+    select = cursor.execute("SELECT id, name FROM  users;")
+    for i in select:
+        print(i)
+        lista.append(i)
+
+print(lista)
+# [(1, 'John'), (2, 'John'), (3, 'Alice'), (4, 'Tom'), (5, 'Radek'), (6, 'John'), (7, 'Alice'), (8, 'Tom'), (9, 'Radek'), (10, 'John'), (11, 'Alice'), (12, 'Tom'), (13, 'Radek')]
+
+pprint(lista)
+# [(1, 'John'),
+#  (2, 'John'),
+#  (3, 'Alice'),
+#  (4, 'Tom'),
+#  (5, 'Radek'),
+#  (6, 'John'),
+#  (7, 'Alice'),
+#  (8, 'Tom'),
+#  (9, 'Radek'),
+#  (10, 'John'),
+#  (11, 'Alice'),
+#  (12, 'Tom'),
+#  (13, 'Radek'),
+#  (14, 'John'),
+#  (15, 'Alice'),
+#  (16, 'Tom'),
+#  (17, 'Radek')]
