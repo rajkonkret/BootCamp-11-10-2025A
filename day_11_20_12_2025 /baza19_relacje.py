a7b1cbce6bb1511771faf3f0b3b39d903d9ff9ea
radoslawjaniak <rajkokret660@gmail.com>
2025-12-20 13:52:09 +0100

    [11 26] update baza18_diagram_parents.py

---
+++ day_11_20_12_2025 /baza19_relacje.py
@@ -0,0 +1,10 @@
+# Jeden-do-jednego (1:1): Jeden rekord w tabeli A jest powiązany z maksymalnie jednym rekordem w tabeli B
+# (np. pracownik i jego samochód służbowy, jeśli ma tylko jeden).
+# ----
+# Jeden-do-wielu (1:N): Jeden rekord w tabeli A może być powiązany z wieloma rekordami w tabeli B
+# (np. jeden klient ma wiele zamówień).
+# ----
+# Wiele-do-wielu (N:M): Wiele rekordów z tabeli A może być powiązanych z wieloma rekordami z tabeli B
+# (np. wiele produktów może występować w wielu zamówieniach).
+# Wymaga to często dodatkowej tabeli pośredniczącej (tabeli łączącej)
+
