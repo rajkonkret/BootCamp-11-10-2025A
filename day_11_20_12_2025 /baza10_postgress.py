2679f0599a5f5b16d1891be17bea0afe160669ac
radoslawjaniak <rajkokret660@gmail.com>
2025-12-20 09:28:03 +0100

    [11 1] add baza_10_postgres

---
+++ day_11_20_12_2025 /baza10_postgress.py
@@ -0,0 +1,12 @@
+# docker -v  - wersja dockera
+#  docker ps - lista aktywnych kontenerów
+# docker ps -a  - lista wszystkich kontenerów (aktywnych i nieaktywnych)
+
+# docker run --name my-postgres1 -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 -d postgres
+# psql -U myuser -d mydatabase -h localhost -p 5432 - logowanie do bazy mydatabase w kontenerze
+
+#  docker stop my-postgres1 - zatrzymanie kontenera o nazwie my-postgres1
+# docker start my-postgres1 - uruchomienie kontenera o nazwie my-postgres1
+
+# pgAdmn - program do zarządzania bazą postgres
+# localhost - 127.0.0.1
