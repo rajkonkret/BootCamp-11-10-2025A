1c553d2cb2aa567e94f72cbd19c2efaf8242dc6c
radoslawjaniak <rajkokret660@gmail.com>
2025-12-20 12:31:01 +0100

    [11 15] update baza16 context manager

--- day_11_20_12_2025 /baza16_context_manager.py
+++ day_11_20_12_2025 /baza16_context_manager.py
@@ -1,2 +1,24 @@
 # context manager - narzędzie ułątwiające pracę np.:  z plikami, bazami
 # with - context manager w pythonie
+# __enter__ - wykonuje się przy uruchomieniu
+# __exit__ - wykonuje się przy wyjsciu(niezależnie czy jest bład czy nie)
+import sqlite3
+
+
+class SQLiteDBContextManager:
+    def __init__(self, db_name):
+        """
+        Metoda inicjalizująca, konstruktor
+        :param db_name:
+        """
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):  # wykonuje się zawsze
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
