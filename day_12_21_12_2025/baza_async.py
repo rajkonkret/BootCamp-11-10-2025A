8e7b227713025b664d823ff423b7f44508413d03
radoslawjaniak <rajkokret660@gmail.com>
2025-12-21 11:54:50 +0100

    [12 24] update baza_async.py

--- day_12_21_12_2025/baza_async.py
+++ day_12_21_12_2025/baza_async.py
@@ -12,3 +12,29 @@ class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String)
+
+
+engine = create_async_engine("sqlite+aiosqlite:///async.db", echo=True)
+Session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with Session() as session:
+        session.add_all([User(name="Ala"), User(name="Ola")])
+
+        await session.commit()
+
+        result = await session.execute(select(User))
+        for user in result.scalars():
+            print(f"[ASYNC] Użytkownik: {user.name}")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+# pip install aiosqlite
+# pip install greenlet
+# python -m pip install aiosqlite
+# python -m pip install greenlet
