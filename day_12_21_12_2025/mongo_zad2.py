2b46946b00b4db94a5be7eb55aa91e1dd8d92314
radoslawjaniak <rajkokret660@gmail.com>
2025-12-21 14:24:50 +0100

    [12 41] update mongo_zad2.py

--- day_12_21_12_2025/mongo_zad2.py
+++ day_12_21_12_2025/mongo_zad2.py
@@ -3,3 +3,12 @@ import datetime
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017")
+
+db = client['przykladowa_baza']
+kolekcja = db['uzytkownicy']
+
+kolekcja.insert_one(
+    {'imie': "Jan", "nazwisko": "Kowalski", "wiek": 30}
+)
+
+client.close()
