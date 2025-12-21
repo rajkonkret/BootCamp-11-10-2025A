0f30cdb3aa948b7e26c9d46645da1ce97d85fe94
radoslawjaniak <rajkokret660@gmail.com>
2025-12-21 10:44:25 +0100

    [12 15] update baza_krok2.py.py

---
+++ day_12_21_12_2025/baza_krok_2_transakcja.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from baza_krok1 import User, Post
+
+# docker run -d --name mysql_db1 -e MYSQL_ROOT_PASSWORD=abc123 -e MYSQL_DATABASE=baza -p 3306:3306 mysql:8.0
+
+# pip install pymysql - musimy doinstalowac pymysql
+# pip install cryptography - mysql wymaga szyfrowania haseł
+DATABASE_URI = "mysql+pymysql://root:abc123@localhost:3306/baza"
+
+engine = create_engine(DATABASE_URI, echo=True)
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+try:
+    new_user = User(name="Jan kowalski", email="jan.kowalski@wp.pl")
+    session.add(new_user)
+    # print(5 / 0)
+    new_post = Post(title="Pierwszy post", content="To jest treść pierwszego posta", user=new_user)
+    session.add(new_post)
+
+    session.commit()
+except Exception as e:
+    print("Bład:", e)
+    session.rollback()  # wycofanie zmian w przypadku błedu
+
+finally:
+    session.close()
