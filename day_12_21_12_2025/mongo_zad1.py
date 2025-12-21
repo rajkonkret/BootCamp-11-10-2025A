02ee14ff704f72ba5e52f0931ebad96e3240f5f4
radoslawjaniak <rajkokret660@gmail.com>
2025-12-21 13:54:50 +0100

    [12 37] update mongo_zad1.py

---
+++ day_12_21_12_2025/mongo_zad1.py
@@ -0,0 +1,5 @@
+# MongoDB, popularnej, nierelacyjnej bazy danych NoSQL, która przechowuje dane w elastycznych,
+# podobnych do JSON dokumentach,
+# oferując skalowalność i wydajność dla dużych zbiorów danych, z pominięciem tradycyjnych tabel i wierszy
+# https://www.ovhcloud.com/pl/learn/what-is-mongodb/
+
