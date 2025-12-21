45e9ac0065bf74697f18b184cc0e47526b4a3954
radoslawjaniak <rajkokret660@gmail.com>
2025-12-21 14:09:27 +0100

    [12 38] update mongo_zad1.py

--- day_12_21_12_2025/mongo_zad1.py
+++ day_12_21_12_2025/mongo_zad1.py
@@ -3,3 +3,10 @@
 # oferując skalowalność i wydajność dla dużych zbiorów danych, z pominięciem tradycyjnych tabel i wierszy
 # https://www.ovhcloud.com/pl/learn/what-is-mongodb/
 
+# docker pull mongo
+# docker run --name mongodb1 -d -p 27017:27017 mongo
+
+#  pip install --upgrade pymongo
+import pymongo
+
+
