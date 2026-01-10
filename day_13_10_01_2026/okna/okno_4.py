2429086cf26ae182064814836478bb8cd6e517b0
radoslawjaniak <rajkokret660@gmail.com>
2026-01-10 09:36:37 +0100

    [13 7] update okno_4.py

---
+++ day_13_10_01_2026/okna/okno_4.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+def show_text():
+    text = entry.get()
+    print(f"Wprowadzony tekst: {text}")
+
+
+app = tk.Tk()
+app.title("Pole wprowadzania")
+
+# definiowanie i umieszczanie na oknie pola wprowadzania
+entry = tk.Entry(app)
+entry.pack()
+
+# Button
+button = tk.Button(app, text="Pokaż tekst", command=show_text)
+button.pack()
+
+app.mainloop()
