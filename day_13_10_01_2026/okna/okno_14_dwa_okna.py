62ee5970a0e058eba3b3fcc5a91908c1c7e18347
radoslawjaniak <rajkokret660@gmail.com>
2026-01-10 11:41:49 +0100

    [13 28] update okno_14_dwa_okna.py

--- day_13_10_01_2026/okna/okno_14_dwa_okna.py
+++ day_13_10_01_2026/okna/okno_14_dwa_okna.py
@@ -6,4 +6,10 @@ root.title("Głowne okno")
 label1 = tk.Label(root, text="To jest główne okno")
 label1.pack(padx=10, pady=10)
 
+second_window = tk.Toplevel(root)
+second_window.title("Drugie okno")
+
+label2 = tk.Label(second_window, text="To jest drugie okno")
+label2.pack(padx=10, pady=10)
+
 root.mainloop()
