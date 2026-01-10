de8e53b526f6977a088aacf11d639aadd53d418e
radoslawjaniak <rajkokret660@gmail.com>
2026-01-10 11:20:20 +0100

    [13 24] update okno_12a_radiobutton.py

--- day_13_10_01_2026/okna/okno_12a_radiobutton.py
+++ day_13_10_01_2026/okna/okno_12a_radiobutton.py
@@ -22,6 +22,9 @@ class MyGui:
         self.radio_var = tk.IntVar()
         self.radio_var.set(1)  # ustawiamy jako aktywny pierwszy wybór
 
+        self.cb_var = tk.IntVar()
+        self.cb_var.set(0)
+
         my_font = tkfont.Font(family="Helvetica", size=18, weight="bold")
 
         self.label1 = tk.Label(self.top_frame, text="Start", font=my_font)
@@ -29,12 +32,14 @@ class MyGui:
         self.rb1 = tk.Radiobutton(self.top_frame, text="Opcja 1", variable=self.radio_var, value=1)
         self.rb2 = tk.Radiobutton(self.top_frame, text="Opcja 2", variable=self.radio_var, value=2)
         self.rb3 = tk.Radiobutton(self.top_frame, text="Opcja 3", variable=self.radio_var, value=3)
+        self.rb4 = tk.Checkbutton(self.top_frame, text="opcja 4", variable=self.cb_var)
 
         self.label1.pack()
 
         self.rb1.pack()
         self.rb2.pack()
         self.rb3.pack()
+        self.rb4.pack()
 
         self.top_frame.pack()
 
