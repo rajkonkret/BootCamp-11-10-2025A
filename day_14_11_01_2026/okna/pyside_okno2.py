a6425eb0036140f4e30e50ec5ea627e52aace127
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 08:30:08 +0100

    [14 06] update pyside_okno2.py

--- day_14_11_01_2026/okna/pyside_okno2.py
+++ day_14_11_01_2026/okna/pyside_okno2.py
@@ -7,3 +7,13 @@ if __name__ == '__main__':
     # wypisanie dostepnych styli
     print("Style available:", QStyleFactory.keys())
     # Style available: ['macOS', 'Windows', 'Fusion']
+
+    # zastosowanie stylu
+    app.setStyle(QStyleFactory.create("Fusion"))
+
+    window = QWidget()
+    window.setWindowTitle("Demo z Fusion")
+    window.resize(300, 200)
+    window.show()
+
+    sys.exit(app.exec())
