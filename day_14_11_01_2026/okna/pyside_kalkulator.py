0abd778959e36cb26ed8082def49c70b5b1f3d49
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 09:47:16 +0100

    [14 22] update pyside_kalkulator.py

--- day_14_11_01_2026/okna/pyside_kalkulator.py
+++ day_14_11_01_2026/okna/pyside_kalkulator.py
@@ -10,6 +10,60 @@ class Calculator(QWidget):
 
         self.setWindowTitle("Calculator")
 
+        # dodanie ikonki
+        self.setWindowIcon(QIcon(QPixmap("calculator.png")))
+
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
+
+        self.result_felds = QLineEdit()
+        self.grid.addWidget(self.result_felds, 0, 0, 1, 4)  # jeden wiersz, cztery kolumny
+
+        self.create_button("7", 1, 0)
+        self.create_button("8", 1, 1)
+        self.create_button("9", 1, 2)
+        self.create_button("/", 1, 3)
+
+        self.create_button("4", 2, 0)
+        self.create_button("5", 2, 1)
+        self.create_button("6", 2, 2)
+        self.create_button("*", 2, 3)
+
+        self.create_button("1", 3, 0)
+        self.create_button("2", 3, 1)
+        self.create_button("3", 3, 2)
+        self.create_button("-", 3, 3)
+
+        self.create_button("0", 4, 0)
+        self.create_button(".", 4, 1)
+        self.create_button("=", 4, 2)
+        self.create_button("+", 4, 3)
+
+        self.create_button("C", 5, 0)
+
+        self.result_felds.setFocus()
+        self.setLayout(self.grid)
+
+    def create_button(self, text, row, column):
+        button = QPushButton(text)
+        button.clicked.connect(lambda: self.button_click(text))
+        self.grid.addWidget(button, row, column)
+
+    def button_click(self, text):
+        if text == "=":
+            self.calculate()
+        elif text == "C":
+            self.result_felds.setText("")
+        else:
+            self.result_felds.setText(self.result_felds.text() + text)
+
+    def calculate(self):
+        try:
+            result = eval(self.result_felds.text())  # wykona działnie, zwróci wynik, niebezpieczne
+            self.result_felds.setText(str(result))
+        except:
+            self.result_felds.setText("Error !!!")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
