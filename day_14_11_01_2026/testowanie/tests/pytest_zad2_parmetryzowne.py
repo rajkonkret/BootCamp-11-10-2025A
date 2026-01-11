7349c298b1b94c620d32198ef2dfbf16237c34a3
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 11:02:35 +0100

    [14 27] update pytest_zad2_parmetryzowne.py

--- day_14_11_01_2026/testowanie/tests/pytest_zad2_parmetryzowne.py
+++ day_14_11_01_2026/testowanie/tests/pytest_zad2_parmetryzowne.py
@@ -1,3 +1,13 @@
 # TDD  - Test-Driven Development - czyli programowanie sterowane testami
 # Jest to technika tworzenia oprogramowania,
 # w której testy jednostkowe są pisane przed napisaniem kodu implementacyjnego
+
+import pytest
+
+
+def calculate_square(num):
+    return num ** 2
+
+
+def test_squre_1():
+    assert calculate_square(2) == 4
