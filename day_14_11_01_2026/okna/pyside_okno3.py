bcaef15ba62c54f8fc94f60a149a8c7ff5bf7d90
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 08:38:03 +0100

    [14 8] update pyside_okno3.py

--- day_14_11_01_2026/okna/pyside_okno3.py
+++ day_14_11_01_2026/okna/pyside_okno3.py
@@ -3,3 +3,31 @@ from PySide6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout,
     QLineEdit, QLabel, QPushButton, QStyleFactory
 )
+
+
+# funkcja uruchamiana po nacisnięciu buttona
+def show_text():
+    pass
+
+
+app = QApplication(sys.argv)
+
+print(QStyleFactory.keys())  # ['macOS', 'Windows', 'Fusion']
+app.setStyle("Fusion")
+
+dialog = QWidget()
+dialog.setWindowTitle("Okno z polem tekstowym")
+dialog.setGeometry(100, 100, 300, 150)
+
+# pole tekstowe
+textbox = QLineEdit()
+textbox.setPlaceholderText("Wpisz coś tutaj...")
+
+# pole labelka
+label = QLabel("Tekst pojawi się tutaj")
+
+# button
+button = QPushButton("Wyświetl tekst")
+
+# podłaczenie przycisku z funkcją
+button.clicked.connect(show_text)  # przekazujemy referencję
