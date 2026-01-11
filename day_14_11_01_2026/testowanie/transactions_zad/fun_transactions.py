8bb01af1249773d0bdbf81fe854fbc81a5ecaaf0
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 12:49:40 +0100

    [14 38] update fun_transactions.py

--- day_14_11_01_2026/testowanie/transactions_zad/fun_transactions.py
+++ day_14_11_01_2026/testowanie/transactions_zad/fun_transactions.py
@@ -9,3 +9,27 @@ transactions = [
     {"id": 6, "type": "expense", "amount": 400, "currency": "EUR"},
     {"id": 7, "type": "income", "amount": 100, "currency": "EUR"},
 ]
+
+
+def filter_transactions(transactions, transaction_type):
+    pass
+
+
+def map_transactions(transactions, currency):
+    pass
+
+
+def reduce_transactions(transactions):
+    pass
+
+
+def process_transactions(transactions, transaction_type, currency):
+    filtered = filter_transactions(transactions, transaction_type)
+    mapped = map_transactions(filtered, currency)
+    total = reduce_transactions(mapped)
+
+    return total
+
+
+if __name__ == '__main__':
+    print(process_transactions(transactions, "expense", "EUR"))
