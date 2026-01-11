8e1e13dde2b4aa9f9c21dfeadd69d4ea354b4694
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 09:04:38 +0100

    [14 17] update pyside_okno6_grid.py

--- day_14_11_01_2026/okna/pyside_okno6_grid.py
+++ day_14_11_01_2026/okna/pyside_okno6_grid.py
@@ -12,6 +12,14 @@ window.setWindowTitle("Grid Layout")
 
 layout = QGridLayout()
 
+layout.addWidget(QPushButton("Przycisk (0,0)"), 0, 0)
+layout.addWidget(QPushButton("Przycisk (0,1)"), 0, 1)
+layout.addWidget(QPushButton("Przycisk (0,2)"), 0, 2)
+layout.addWidget(QPushButton("Przycisk (1,0)"), 1, 0)
+layout.addWidget(QPushButton("Przycisk (1,1)"), 1, 1)
+layout.addWidget(QPushButton("Przycisk (1,2)"), 1, 2)
+layout.addWidget(QPushButton("Przycisk (2,0)"), 2, 0)
+
 window.setLayout(layout)
 
 window.show()
