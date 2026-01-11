d548e245393da165ce2b2a24f4c0e2e66cff6b43
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 10:58:39 +0100

    [14 26] update pytest_zad2_parmetryzowne.py

---
+++ day_14_11_01_2026/testowanie/tests/pytest_zad2_parmetryzowne.py
@@ -0,0 +1,3 @@
+# TDD  - Test-Driven Development - czyli programowanie sterowane testami
+# Jest to technika tworzenia oprogramowania,
+# w której testy jednostkowe są pisane przed napisaniem kodu implementacyjnego
