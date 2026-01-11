302fffbcb35f337b9997ab331864998949cabf65
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 11:06:47 +0100

    [14 28] update pytest_zad2_parmetryzowne.py

--- day_14_11_01_2026/testowanie/tests/pytest_zad2_parmetryzowne.py
+++ day_14_11_01_2026/testowanie/tests/pytest_zad2_parmetryzowne.py
@@ -11,3 +11,15 @@ def calculate_square(num):
 
 def test_squre_1():
     assert calculate_square(2) == 4
+
+
+# test parametryzowany
+# uruchomiony wielokrotnie z róznymi argumentami
+
+@pytest.mark.parametrize("input, expected_output", [(2, 4), (3, 9), (4, 16)])
+def test_square_calculate_parametrized(input, expected_output):
+    assert calculate_square(input) == expected_output
+# pytest_zad2_parmetryzowne.py::test_squre_1 PASSED                                                                                             [ 25%]
+# pytest_zad2_parmetryzowne.py::test_square_calculate_parametrized[2-4] PASSED                                                                  [ 50%]
+# pytest_zad2_parmetryzowne.py::test_square_calculate_parametrized[3-9] PASSED                                                                  [ 75%]
+# pytest_zad2_parmetryzowne.py::test_square_calculate_parametrized[4-16] PASSED                                                                 [100%]
