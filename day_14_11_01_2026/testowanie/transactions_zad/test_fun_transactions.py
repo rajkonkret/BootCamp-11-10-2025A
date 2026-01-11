336cd72a1d5f0de8079cdc8def529000215a2e46
radoslawjaniak <rajkokret660@gmail.com>
2026-01-11 13:05:53 +0100

    [14 42] update fun_transactions.py

--- day_14_11_01_2026/testowanie/transactions_zad/test_fun_transactions.py
+++ day_14_11_01_2026/testowanie/transactions_zad/test_fun_transactions.py
@@ -23,3 +23,22 @@ def test_filter_transactions_income():
 
     assert ft.filter_transactions(ft.transactions, "income") == expected_list
     # test_fun_transactions.py::test_filter_transactions_income PASSED
+
+
+def test_process_transactions_expense_eur():
+    assert ft.process_transactions(ft.transactions, "expense", "EUR") == 400
+# (.venv) radoslawjaniak@mac transactions_zad % pytest -v
+# ================================================================ test session starts ================================================================
+# platform darwin -- Python 3.13.7, pytest-9.0.2, pluggy-1.6.0 -- /Users/radoslawjaniak/PycharmProjects/BootCamp-11-10-2025A/.venv/bin/python
+# cachedir: .pytest_cache
+# rootdir: /Users/radoslawjaniak/PycharmProjects/BootCamp-11-10-2025A/day_14_11_01_2026/testowanie/transactions_zad
+# plugins: anyio-4.12.0
+# collected 4 items
+#
+# test_fun_transactions.py::test_map_transactions_usd PASSED                                                                                    [ 25%]
+# test_fun_transactions.py::test_reduce_transactions PASSED                                                                                     [ 50%]
+# test_fun_transactions.py::test_filter_transactions_income PASSED                                                                              [ 75%]
+# test_fun_transactions.py::test_process_transactions_expense_eur PASSED                                                                        [100%]
+#
+# ================================================================= 4 passed in 0.02s =================================================================
+# (.venv) radoslawjaniak@mac transactions_zad %
